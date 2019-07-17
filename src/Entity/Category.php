490fa40727fae466f722b2008fd9c50e04f6406d
Alex Chesnay <me@alex-chesnay.fr>
2019-07-17 19:58:44 +0200

    feat<category>: Validation des données

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
@@ -19,6 +20,8 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3, max=20)
      */
     private $name;
 
