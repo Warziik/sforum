6ffaeecf20e183ff9f039eba0ed10897d2edbce4
Alex Chesnay <me@alex-chesnay.fr>
2019-07-17 18:18:37 +0200

    Premier commit!

---
+++ README.md
@@ -0,0 +1,2 @@
+# SForum
+Un forum de discussion développé avec le framework [Symfony](https://symfony.com/).
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController {
+    /**
+     * @Route("/", name="home")
+     */
+    public function index() {
+        return $this->render('index.html.twig');
+    }
+}
