67d44611a379f44fa99ceb7cd88446cc4caeb841
Alex Chesnay <me@alex-chesnay.fr>
2019-07-21 15:40:33 +0200

    feat<topicresponse>: Validation des données

--- src/Entity/TopicResponse.php
+++ src/Entity/TopicResponse.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TopicResponseRepository")
@@ -19,6 +20,8 @@ class TopicResponse
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2)
      */
     private $content;
 
