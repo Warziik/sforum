2cf76c5905e2cc7d6ba7ad385289be21032c7b48
Alex Chesnay <me@alex-chesnay.fr>
2019-08-11 22:43:01 +0200

    feat<topic>: Pagination des sujets

--- src/Controller/Forum/SubcategoryController.php
+++ src/Controller/Forum/SubcategoryController.php
@@ -2,7 +2,9 @@
 namespace App\Controller\Forum;
 
 use App\Repository\SubcategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubcategoryController extends AbstractController {
@@ -20,13 +22,21 @@ class SubcategoryController extends AbstractController {
      * @Route("/{slug}-{id}", name="forum.subcategory", methods={"GET"}, requirements={"slug"="^[a-zA-Z0-9-_]+$", "id"="\d+"})
      * @param string $slug
      * @param int $id
+     * @param Request $request
+     * @param PaginatorInterface $paginator
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function show(string $slug, int $id) {
+    public function show(string $slug, int $id, Request $request, PaginatorInterface $paginator) {
         $subcategory = $this->subcategoryRepository->find($id);
         if (is_null($subcategory) || $slug != $subcategory->getSlug()) {
             return $this->redirectToRoute('home');
         }
-        return $this->render('forum/subcategory.html.twig', ['subcategory' => $subcategory]);
+
+        $pagination = $paginator->paginate(
+            $subcategory->getTopics(),
+            $request->query->getInt('page', 1),
+            12,
+        );
+        return $this->render('forum/subcategory.html.twig', compact('subcategory', 'pagination'));
     }
 }
